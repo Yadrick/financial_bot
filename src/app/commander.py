b15091f3f1a6cd086f2632b95097b08119465e65
Yadrick <iarik_iarik@list.ru>
2024-04-02 13:36:00 +0300

    added a class for  working with commands

--- src/app/commander.py
+++ src/app/commander.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
-from ..app.machine_state import StateMachine, State
+from ..app.machine_state import StateMachine
+from ..app.machine_commands import CommandsMachine, Commands
 from ..client.client import ClientInformation
 from ..client.interface import BaseClient
 from ..services.income_service import MakeIncomeService
@@ -11,51 +12,86 @@ from ..config.config import STARTING_MESSAGE, WRONG_INPUT
 class ClientStateInfo:
     state: StateMachine
     last_info: dict
+    command: CommandsMachine
 
 
 class Commander:
     """
     Class processes the received messages from the Telegram api and then launches the necessary services
     """
+
     def __init__(self, client: BaseClient, make_income_service: MakeIncomeService):
         self.client = client
         self.make_income_service = make_income_service
         self.last_update_id = 0
 
-    def manage(self, clients: dict[int: ClientStateInfo]):
+    def manage(self, clients: dict[int:ClientStateInfo]):
         updates = self.client.get_update(self.last_update_id)
 
         if updates:
             for update in updates:
-                client_information = ClientInformation(  # TODO: Check fields in 'update'
-                    update['message']['from']['first_name'],
-                    update['update_id'],
-                    update['message']['chat']['id'],
-                    update['message']['text'],
+                client_information = (
+                    ClientInformation(  # TODO: Check fields in 'update'
+                        update["message"]["from"]["first_name"],
+                        update["update_id"],
+                        update["message"]["chat"]["id"],
+                        update["message"]["text"],
+                    )
                 )
-                self.last_update_id = update['update_id'] + 1
+                self.last_update_id = update["update_id"] + 1
 
                 if client_information.chat_id not in clients.keys():
                     state = StateMachine()
+                    command = CommandsMachine()
                     last_info = {}
-                    clients[client_information.chat_id] = ClientStateInfo(state, last_info)
+                    clients[client_information.chat_id] = ClientStateInfo(
+                        state, last_info, command
+                    )
+                    print(f"if: {command.current_command}")
                 else:
                     client_state_info = clients[client_information.chat_id]
                     state = client_state_info.state
+                    command = client_state_info.command
                     last_info = client_state_info.last_info
+                    print(f"else: {command.current_command}")
 
-                if client_information.text in ('/start', '/help'):
-                    self.client.send_message(client_information.chat_id, STARTING_MESSAGE)
-                elif client_information.text == '/make_income' or (
-                        clients[client_information.chat_id].state.current_state != State.START
+                # print(clients[client_information.chat_id].command.current_command)
+
+                if client_information.text in ("/start", "/help"):
+                    self.client.send_message(
+                        client_information.chat_id, STARTING_MESSAGE
+                    )
+                elif (
+                    client_information.text == "/make_income"
+                    or clients[client_information.chat_id].command.current_command
+                    == Commands.MAKE_INCOME
                 ):
-                    state, last_info = self.make_income_service.make_income(
-                        update['message']['text'],
+                    state, last_info, command = self.make_income_service.make_income(
+                        update["message"]["text"],
                         state,
                         last_info,
-                        update['message']['chat']['id']
+                        update["message"]["chat"]["id"],
+                        command,
                     )
-                    clients[update['message']['chat']['id']] = ClientStateInfo(state, last_info)
+                    clients[client_information.chat_id] = ClientStateInfo(
+                        state, last_info, command
+                    )
                 else:
                     self.client.send_message(client_information.chat_id, WRONG_INPUT)
                 return clients
+
+    def init_user_tracking(
+        client_information: ClientInformation, clients: dict[int:ClientStateInfo]
+    ):
+        if client_information.chat_id not in clients.keys():
+            state = StateMachine()
+            command = CommandsMachine()
+            last_info = {}
+            clients[client_information.chat_id] = ClientStateInfo(
+                state, last_info, command
+            )
+        else:
+            client_state_info = clients[client_information.chat_id]
+            state = client_state_info.state
+            command = client_state_info.command
+            last_info = client_state_info.last_info
--- src/services/income_service.py
+++ src/services/income_service.py
@@ -1,11 +1,12 @@
 from ..app.machine_state import StateMachine, State
+from ..app.machine_commands import CommandsMachine, Commands
 from ..client.interface import BaseClient
 from ..repository.interface import BaseRepository
 from ..config.config import (
     MESSAGE_ENTER_DATE,
     MESSAGE_ENTER_CATEGORY,
     MESSAGE_ENTER_AMOUNT,
-    MESSAGE_SUCCESS
+    MESSAGE_SUCCESS,
 )
 
 
@@ -13,29 +14,39 @@ class MakeIncomeService:
     """
     Service for making income
     """
+
     def __init__(self, client: BaseClient, repository: BaseRepository):
         self.client = client
         self.repository = repository
 
-    def make_income(self, text: str, state: StateMachine, last_info: dict, chat_id: int) -> tuple[StateMachine, dict]:
+    def make_income(
+        self,
+        text: str,
+        state: StateMachine,
+        last_info: dict,
+        chat_id: int,
+        command: CommandsMachine,
+    ) -> tuple[StateMachine, dict]:
         if state.current_state == State.START:
             self.client.send_message(chat_id, MESSAGE_ENTER_CATEGORY)
             state.change_state(State.RECEIVED_CATEGORY)
-            return state, last_info
+            command.change_command(Commands.MAKE_INCOME)
+            return state, last_info, command
         elif state.current_state == State.RECEIVED_CATEGORY:
-            last_info['category'] = text
+            last_info["category"] = text
             self.client.send_message(chat_id, MESSAGE_ENTER_DATE)
             state.change_state(State.RECEIVED_DATE)
-            return state, last_info
+            return state, last_info, command
         elif state.current_state == State.RECEIVED_DATE:
-            last_info['date'] = text
+            last_info["date"] = text
             self.client.send_message(chat_id, MESSAGE_ENTER_AMOUNT)
             state.change_state(State.RECEIVED_AMOUNT)
-            return state, last_info
+            return state, last_info, command
         elif state.current_state == State.RECEIVED_AMOUNT:
-            last_info['amount'] = text
+            last_info["amount"] = text
             self.client.send_message(chat_id, MESSAGE_SUCCESS)
-            self.repository.save(last_info)
+            # self.repository.save(last_info)
             state.change_state(State.START)
+            command.change_command(Commands.NONE)
             last_info = {}
-            return state, last_info
+            return state, last_info, command
