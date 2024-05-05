60f09d142ba7153f15fff0d790a9d9b83a41a600
Yadrick <iarik_iarik@list.ru>
2024-05-05 22:16:46 +0300

    updating the database structure + implementing a method for deleting categories

--- src/repository/repository.py
+++ src/repository/repository.py
@@ -26,10 +26,11 @@ INSERT INTO users (id, name) SELECT %s, %s WHERE NOT EXISTS (SELECT 1 FROM users
                     last_info.chat_id,
                 ),
             )
-            self.connect.commit()
-            cursor.close()
         except psycopg.Error as e:
             print("Error:", e)
+        finally:
+            self.connect.commit()
+            cursor.close()
 
     def save_category(self, last_info: ClientLastInfo, type_category: str):
         try:
@@ -46,29 +47,36 @@ INSERT INTO categories (name, type, user_id) SELECT %s, %s, %s WHERE NOT EXISTS 
                     last_info.category,
                 ),
             )
-            self.connect.commit()
-            cursor.close()
         except psycopg.Error as e:
             print("Error:", e)
+        finally:
+            self.connect.commit()
+            cursor.close()
 
-    def save_transaction(self, last_info: ClientLastInfo):
+    def save_transaction(self, last_info: ClientLastInfo, type_category: str):
         try:
             cursor = self.connect.cursor()
-            query_trans = "INSERT INTO transactions (date, amount_money, category_name, user_id) VALUES ( %s, %s, %s, %s);"
+            query_id_category = "SELECT id FROM categories WHERE name = %s AND user_id = %s AND type = %s"
+            cursor.execute(
+                query_id_category,
+                (last_info.category, last_info.chat_id, type_category),
+            )
+            category_id = cursor.fetchone()
+            query_trans = "INSERT INTO transactions (date, amount_money, category_id, user_id) VALUES ( %s, %s, %s, %s);"
             cursor.execute(
                 query_trans,
                 (
                     last_info.date,
                     last_info.amount,
-                    last_info.category,
+                    category_id[0],
                     last_info.chat_id,
                 ),
             )
-            self.connect.commit()
-            cursor.close()
-
         except psycopg.Error as e:
             print("Error:", e)
+        finally:
+            self.connect.commit()
+            cursor.close()
 
     def get_categories(self, type_category: str, chat_id: str) -> Categories:
         """
@@ -77,15 +85,53 @@ INSERT INTO categories (name, type, user_id) SELECT %s, %s, %s WHERE NOT EXISTS 
         try:
             cursor = self.connect.cursor()
             cursor.execute(
-                f"SELECT name FROM categories WHERE type = '{type_category}' AND (user_id IS NULL OR user_id = {chat_id}) "
+                f"SELECT name FROM categories WHERE type = '{type_category}' AND user_id = {chat_id} "
             )
 
             categories = [row[0] for row in cursor.fetchall()]
-
-            cursor.close()
 
             return Categories(categories)
 
         except psycopg.Error as e:
             print("Error:", e)
             return None
+        finally:
+            cursor.close()
+
+    def delete_category_and_related_transactions(
+        self, last_info: ClientLastInfo, type_category: str
+    ):
+        """
+        пу-пу-пу
+        """
+        try:
+            cursor = self.connect.cursor()
+            query = "SELECT id FROM categories WHERE name = %s AND user_id = %s AND type = %s"
+            query_delete_transactions = (
+                "DELETE FROM transactions WHERE category_id = %s AND user_id = %s"
+            )
+            query_delete_category = "DELETE FROM categories WHERE id = %s"
+            cursor.execute(
+                query,
+                (last_info.category, last_info.chat_id, type_category),
+            )
+            category_id = cursor.fetchone()
+            if category_id:
+                cursor.execute(
+                    query_delete_transactions,
+                    (
+                        category_id[0],
+                        last_info.chat_id,
+                    ),
+                )
+                cursor.execute(
+                    query_delete_category,
+                    (category_id),
+                )
+            return category_id
+
+        except psycopg.Error as e:
+            print("Error:", e)
+        finally:
+            self.connect.commit()
+            cursor.close()
